1fe9da01158c1812a70dcd8dcac7ea88a82777a4
Tom van den Bogaart <TomvandenBogaart1@hotmail.com>
2021-02-10 14:58:02 +0100

    Appbar + Worldmap added
    Appbar from material UI and WorldMap from simple maps added to the project.

--- cao-frontend/src/App.js
+++ cao-frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import MenuAppBar from "./Components/MenuAppBar";
+import MapChart from "./Components/MapChart";
+import ReactTooltip from "react-tooltip";
 
 function App() {
+  const [content, setContent] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MenuAppBar></MenuAppBar>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
