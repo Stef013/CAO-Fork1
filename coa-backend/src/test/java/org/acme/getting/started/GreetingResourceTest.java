a41b060ca87d1efeee7edf401659f2a953370707
I383418 <43238133+vincenzo3945@users.noreply.github.com>
2021-02-11 11:58:35 +0100

    Qwarkus opgezet met tests & eerste get route

--- coa-backend/src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ coa-backend/src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package org.acme.getting.started;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -12,10 +14,20 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
     }
 
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
+    }
 }
