89852ec44c7ace60ac768b17057d3ec09cea777c
Kevin Tinnemans <kevintinnemans@upcmail.nl>
2021-03-08 15:18:24 +0100

    Added backend infrastructure

---
+++ coa-backend/src/main/java/Services/AccountService.java
@@ -0,0 +1,13 @@
+package Services;
+
+import javax.enterprise.context.ApplicationScoped;
+import Models.RegisterModel;
+
+@ApplicationScoped
+public class AccountService {
+
+    public String register(RegisterModel registerModel) {
+
+        return "kanker";
+    }
+}
