6836f0240220c533a9f79e4507462cf3e454f146
i383418 <i383418@fhict.nl>
2021-03-18 13:42:53 +0100

    Functies gecombineerd, module probleem gefixt en eerrste rest api voor service

---
+++ cao-backend/Account_Service/src/main/java/Logic/Registration.java
@@ -0,0 +1,23 @@
+package Logic;
+
+import Model.RegistrationCustomer;
+import Repository.RegistrationRepo;
+
+
+public class Registration {
+
+    RegistrationRepo RR = new RegistrationRepo();
+
+    public boolean registrateCustomer(RegistrationCustomer newCustomer)
+    {
+        if (newCustomer != null && newCustomer.getEmail() != null && !newCustomer.getEmail().isBlank() && newCustomer.getPassword() != null && !newCustomer.getPassword().isBlank() &&
+                newCustomer.getFirstname() != null && !newCustomer.getFirstname().isBlank()&& newCustomer.getLastname() != null && !newCustomer.getLastname().isBlank() &&
+                newCustomer.getNationality() != null && !newCustomer.getNationality().isBlank() && newCustomer.getDateOfBirth() != null)
+        {
+            return RR.RegistrateCustomer(newCustomer);
+        }
+        return false;
+
+    }
+
+}
