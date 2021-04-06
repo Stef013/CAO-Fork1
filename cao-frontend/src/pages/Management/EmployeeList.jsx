2f466cc6afb59d3f31f938a8ddcc6b13a783e00b
StefanV <stefan97vu@gmail.com>
2021-04-06 13:27:36 +0200

    EmployeeList gefixt

--- cao-frontend/src/pages/Management/EmployeeList.jsx
+++ cao-frontend/src/pages/Management/EmployeeList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
 import { Accordion, AccordionDetails, AccordionSummary, AccordionActions, Typography, Button, Divider, Container, Paper, TextField, MenuItem } from '@material-ui/core';
 
@@ -7,7 +7,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import MenuAppBar from "../../Components/MenuAppBar";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
     maincolumn1: {
         flexBasis: '33%'
     },
@@ -59,7 +59,7 @@ const useStyles = makeStyles((theme) => ({
     accordion: {
         width: "90%",
     }
-}));
+});
 
 const roles = [
     {
@@ -72,86 +72,104 @@ const roles = [
     },
 ];
 
-export default function DetailedAccordion() {
-    const classes = useStyles();
+class EmployeeList extends Component {
 
-    return (
-        <div>
-            <MenuAppBar></MenuAppBar>
-            <Typography className={classes.title} align="center" variant="h3" >
-                New Employee Account
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            role: "",
+        };
+
+        this.account = {
+            id: 0,
+            email: "",
+            password: "",
+            firstname: "",
+            lastname: "",
+            role: "",
+        };
+    }
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <MenuAppBar></MenuAppBar>
+                <Typography className={classes.title} align="center" variant="h3" >
+                    Edit Employees
                 </Typography>
-            <Container align="center">
-                <Paper className={classes.paper} >
-                    <Accordion className={classes.accordion} >
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1c-content"
-                            id="panel1c-header"
-                        >
-                            <div className={classes.maincolumn1}>
-                                <Typography >1. Piet Janssen</Typography>
-                            </div>
-                            <div className={classes.maincolumn1}>
-                                <Typography >Piet@janssen.nl</Typography>
-                            </div>
-                            <div className={classes.maincolumn1}>
-                                <Typography >Employee</Typography>
-                            </div>
-                        </AccordionSummary>
-                        <Divider style={{ marginBottom: 20 }} />
-                        <AccordionDetails className={classes.details}>
-                            <div className={classes.column1}>
-                                <Typography className={classes.heading}>Reset Password: </Typography>
-                            </div>
-                            <div className={classes.column2}>
-                                <TextField
-                                    variant="outlined"
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                />
-                            </div>
+                <Container align="center">
+                    <Paper className={classes.paper} >
+                        <Accordion className={classes.accordion} >
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1c-content"
+                                id="panel1c-header"
+                            >
+                                <div className={classes.maincolumn1}>
+                                    <Typography >1. Piet Janssen</Typography>
+                                </div>
+                                <div className={classes.maincolumn1}>
+                                    <Typography >Piet@janssen.nl</Typography>
+                                </div>
+                                <div className={classes.maincolumn1}>
+                                    <Typography >Employee</Typography>
+                                </div>
+                            </AccordionSummary>
+                            <Divider style={{ marginBottom: 20 }} />
+                            <AccordionDetails className={classes.details}>
+                                <div className={classes.column1}>
+                                    <Typography className={classes.heading}>Reset Password: </Typography>
+                                </div>
+                                <div className={classes.column2}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                    />
+                                </div>
+                            </AccordionDetails>
+                            <AccordionDetails className={classes.details}>
+                                <div className={classes.column1}>
+                                    <Typography className={classes.heading}>Change Role:</Typography>
+                                </div>
+                                <div className={classes.column2}>
+                                    <TextField
+                                        id="roleselect"
+                                        select
+                                        fullWidth
+                                        required
+                                        label="Select role"
+                                        name="role"
+                                        align="left"
+                                        variant="outlined"
+                                    >
+                                        {roles.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                                {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+                                </div>
 
-
-                        </AccordionDetails>
-                        <AccordionDetails className={classes.details}>
-                            <div className={classes.column1}>
-                                <Typography className={classes.heading}>Change Role:</Typography>
-                            </div>
-                            <div className={classes.column2}>
-                                <TextField
-                                    id="roleselect"
-                                    select
-                                    fullWidth
-                                    required
-                                    label="Select role"
-                                    name="role"
-                                    align="left"
-                                    variant="outlined"
-                                >
-                                    {roles.map((option) => (
-                                        <MenuItem key={option.value} value={option.value}>
-                                            {option.label}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </div>
-
-
-                        </AccordionDetails>
-                        <Divider />
-                        <AccordionActions>
-                            <Button size="small">Cancel</Button>
-                            <Button size="small" color="primary" variant="contained">
-                                Save
+                            </AccordionDetails>
+                            <Divider />
+                            <AccordionActions>
+                                <Button size="small">Cancel</Button>
+                                <Button size="small" color="primary" variant="contained">
+                                    Save
                             </Button>
-                        </AccordionActions>
-                    </Accordion>
-                </Paper>
-            </Container>
-        </div >
-    );
+                            </AccordionActions>
+                        </Accordion>
+                    </Paper>
+                </Container>
+            </div >
+        )
+    }
 }
+
+export default (withStyles(useStyles)(EmployeeList))
