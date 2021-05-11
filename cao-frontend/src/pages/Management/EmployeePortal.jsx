e74a01b429b8e26d0041567e845ea5af79a16181
StefanV <stefan97vu@gmail.com>
2021-05-11 10:39:34 +0200

    Flight links in portal toegevoegd

--- cao-frontend/src/pages/Management/EmployeePortal.jsx
+++ cao-frontend/src/pages/Management/EmployeePortal.jsx
@@ -9,6 +9,7 @@ const useStyles = (theme) => ({
 
     title: {
         marginTop: 30,
+        marginBottom: 30,
         color: "white"
     },
     card: {
@@ -36,17 +37,23 @@ class EmployeePortal extends Component {
         }
         this.navigateCreation = this.navigateCreation.bind(this);
         this.navigateList = this.navigateList.bind(this);
+        this.navigateSummary = this.navigateSummary.bind(this);
+        this.navigatePlanner = this.navigatePlanner.bind(this);
     }
 
-    componentDidMount() {
-        document.body.style.backgroundColor = "#3166b0"
-    }
     navigateCreation() {
         this.props.history.push('/EmployeeCreation');
     }
     navigateList() {
         this.props.history.push('/EmployeeList');
     }
+    navigateSummary() {
+        this.props.history.push('/FlightSummary');
+    }
+    navigatePlanner() {
+        this.props.history.push('/FlightPlanner');
+    }
+
     render() {
         const { classes } = this.props;
         return (
@@ -82,6 +89,35 @@ class EmployeePortal extends Component {
                                     color="primary"
                                     className={classes.createButton}
                                     onClick={this.navigateList}>View</Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                        <Card className={classes.card} align="center">
+                            <CardContent>
+                                <Typography className={classes.cardTitle} variant="h6" gutterBottom>
+                                    Flight List
+                                </Typography>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.createButton}
+                                    onClick={this.navigateSummary}>View</Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                        <Card className={classes.card} align="center">
+                            <CardContent>
+                                <Typography className={classes.cardTitle} variant="h6" gutterBottom>
+                                    Flight Planner
+                                </Typography>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.createButton}
+                                    onClick={this.navigatePlanner}>View</Button>
                             </CardContent>
                         </Card>
                     </Grid>
